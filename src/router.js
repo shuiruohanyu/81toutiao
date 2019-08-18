1a003ba35fbd4e9b2881a023f242b98c55a95f95
gaoly <shuiruohanyu@foxmail.com>
2019-08-18 18:35:51 +0800

    新建home主页和登录主页

--- src/router.js
+++ src/router.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Login from './views/login'
+import Home from './views/home'
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: Home
-    // }
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home
+    }
     // {
     //   path: '/about',
     //   name: 'about',
